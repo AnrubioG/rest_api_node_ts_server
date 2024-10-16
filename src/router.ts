a942bcc59b4829b7b799a8e0c52c0a60f59ee799
AnubioG <rubioporrasangela@gmail.com>
2024-10-15 22:58:45 -0500

    Documentation with swagger

--- src/router.ts
+++ src/router.ts
@@ -12,9 +12,81 @@ import { handleInputsError } from "./middleware";
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                   id:
+ *                      type: integer
+ *                      description: The Product ID
+ *                      example: 1
+ *                   name:
+ *                      type: string
+ *                      description: The Product name
+ *                      example: Monitor curvo de 49 pulgadas
+ *                   price:
+ *                      type: number
+ *                      description: The Product price
+ *                      example: 300
+ *                   availability:
+ *                      type: boolean
+ *                      description: The Product availability
+ *                      example: true
+ */
+
 // Routing
 
+/**
+ * @swagger
+ * /api/products:
+ *      get:
+ *        summary: Get a list of products
+ *        tags:
+ *            - Products
+ *        descriptios: Return a list of productos
+ *        responses:
+ *                200:
+ *                    description: Successful response
+ *                    content:
+ *                        application/json:
+ *                              schema:
+ *                                  type: array
+ *                                  items:
+ *                                      $ref: '#/components/schemas/Product'
+ */
+
 router.get("/", getProducts);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *      get:
+ *        summary: Get a product by ID
+ *        tags:
+ *            - Products
+ *        descriptios: Return a product based on its unique ID
+ *        parameters:
+ *              - in: path
+ *                name: id
+ *                description: The ID of the product to retrieve
+ *                required: true
+ *                schema:
+ *                    type: integer
+ *        responses:
+ *                200:
+ *                    description: Successful response
+ *                    content:
+ *                        application/json:
+ *                              schema:
+ *                                   $ref: '#/components/schemas/Product'
+ *                404:
+ *                    description: Not found
+ *                400:
+ *                    description: Bad Request - Invalid ID
+ */
 
 router.get(
   "/:id",
@@ -22,6 +94,39 @@ router.get(
   handleInputsError,
   getProductById
 );
+
+/**
+ * @swagger
+ * /api/products:
+ *      post:
+ *        summary: Create a new product
+ *        tags:
+ *            - Products
+ *        description: Returns a new record in the database
+ *        requestBody:
+ *            required: true
+ *            content:
+ *                 application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  example: "Monitor curvo de 49 pulgadas"
+ *                              price:
+ *                                  type: number
+ *                                  example: 300
+ *        responses:
+ *                201:
+ *                    description: Successful response
+ *                    content:
+ *                        application/json:
+ *                              schema:
+ *                                   $ref: '#/components/schemas/Product'
+ *                400:
+ *                  description: Bad Request - Invalid input data
+ *
+ */
 
 router.post(
   "/",
@@ -41,6 +146,51 @@ router.post(
   handleInputsError,
   createProduct
 );
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *      put:
+ *        summary: Updates a product whith user input
+ *        tags:
+ *            - Products
+ *        description: Returns the updated product
+ *        parameters:
+ *              - in: path
+ *                name: id
+ *                description: The ID of the product to retrieve
+ *                required: true
+ *                schema:
+ *                    type: integer
+ *        requestBody:
+ *            required: true
+ *            content:
+ *                 application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  example: "Monitor curvo de 49 pulgadas"
+ *                              price:
+ *                                  type: number
+ *                                  example: 300
+ *                              availability:
+ *                                  type: boolean
+ *                                  example: true
+ *        responses:
+ *                200:
+ *                    description: Successful response
+ *                    content:
+ *                        application/json:
+ *                              schema:
+ *                                   $ref: '#/components/schemas/Product'
+ *                400:
+ *                  description: Bad Request - Invalid ID or Invalid input data
+ *                404:
+ *                  description: Product Not Found
+ *
+ */
 
 router.put(
   "/:id",
@@ -66,12 +216,71 @@ router.put(
   updateProduct
 );
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *      patch:
+ *        summary: Update product availability
+ *        tags:
+ *            - Products
+ *        description: Returns the updated availability
+ *        parameters:
+ *              - in: path
+ *                name: id
+ *                description: The ID of the product to retrieve
+ *                required: true
+ *                schema:
+ *                    type: integer
+ *        responses:
+ *                200:
+ *                    description: Successful response
+ *                    content:
+ *                        application/json:
+ *                              schema:
+ *                                  type: string
+ *                400:
+ *                  description: Bad Request - Invalid ID
+ *                404:
+ *                  description: Product Not Found
+ *
+ */
+
 router.patch(
   "/:id",
   param("id").isInt().withMessage("Id no válido"),
   handleInputsError,
   updateavailability
 );
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *      delete:
+ *        summary: Delete a product by  a given ID
+ *        tags:
+ *            - Products
+ *        description: Return a confirmation massage
+ *        parameters:
+ *              - in: path
+ *                name: id
+ *                description: The ID of the product to delete
+ *                required: true
+ *                schema:
+ *                    type: integer
+ *        responses:
+ *                200:
+ *                    description: Successful response
+ *                    content:
+ *                        application/json:
+ *                              schema:
+ *                                  type: string
+ *                                  value: "Producto eliminado"
+ *                400:
+ *                  description: Bad Request - Invalid ID
+ *                404:
+ *                  description: Product Not Found
+ *
+ */
 
 router.delete(
   "/:id",
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,24 @@
 import express from "express";
-// import colors from "colors";
 import router from "./router";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger";
 import db from "./config/db";
 
 // conexion a la base de datos
 async function connectDB() {
-  try {
-    await db.authenticate();
-    db.sync();
-    // console.log(colors.blue.bold("Conexion exitosa a la base de datos"));
-  } catch (error) {
-    // console
-    //   .log
-    //   colors.red.bold("Hubo un error en la conexion con la base de datos")
-    //   ();
-    // console.log(error);
-  }
+  await db.authenticate();
+  db.sync();
 }
 
 connectDB();
 
 // Instancia de express
 const server = express();
+// leer datos de formularios
 server.use(express.json());
 server.use("/api/products", router);
 
-server.get("/api", (req, res) => {
-  res.json({ msg: "Desde API" });
-});
+// Docs
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default server;
